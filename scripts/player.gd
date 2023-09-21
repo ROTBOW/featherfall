7e3ee69f332ce5b165e4ec1c693a018b363459de
Josiah Leon <sjosiahleon@gmail.com>
2023-09-21 08:38:41 -0700

    WIP: 60e27d7 player token can now walk in according to the input keys WASD

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,24 +4,30 @@ extends CharacterBody2D
 const SPEED = 150.0
 
 func _ready():
-	$AnimatedSprite2D.animation = 'walk'
+	# When the node is loaded, we set the animation is set to the walk animation
+	$Character_Body.animation = 'walk'
+	$Whoosh_Effect.hide()
+
+func _process(delta):
+	if Input.is_action_pressed('left_click'):
+		print('you left clicked!')
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var d_left_right = Input.get_axis("left_action", "right_action")
 	var d_up_down = Input.get_axis('up_action', "down_action")
 	
-	# playing walking animation if there is any movement
+	# Playing walking animation if there is any movement
 	if velocity.length() > 0:
-		$AnimatedSprite2D.play()
+		$Character_Body.play()
 	else:
-		$AnimatedSprite2D.stop()
+		$Character_Body.stop()
 	
 	# if we are moving left or right move us that direction times the speed
 	if d_left_right:
 		velocity.x = d_left_right * SPEED
 		# Flip the animation based on the direction we're moving
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$Character_Body.flip_h = velocity.x < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
